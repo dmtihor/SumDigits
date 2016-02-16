c650366ab47bb8d176623e37c8cc00374f2cb01b
Dmytriv Ihor <dmtihor@gmail.com>
2016-02-16 10:19:47 +0200

    Initial commit

---
+++ src/SumDigits.java
@@ -0,0 +1,15 @@
+
+public class SumDigits {
+    public int sum(int number) {
+
+        int sum = 0;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return Math.abs(sum);
+    }
+}
+
